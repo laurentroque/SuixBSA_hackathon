d916a2de9172812fcc3baa07097d79fea2df57e0
jason7791 <148640284+jason7791@users.noreply.github.com>
2023-10-22 11:31:29 +0200

    Update Bids.jsx
    change price to 100 SUI

--- frontend/src/components/bids/Bids.jsx
+++ frontend/src/components/bids/Bids.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './bids.css'
-import { AiFillHeart,AiOutlineHeart } from "react-icons/ai";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import bids1 from '../../assets/bids1.png'
 import bids2 from '../../assets/bids2.png'
 import bids3 from '../../assets/bids3.png'
@@ -11,7 +11,8 @@ import bids7 from '../../assets/bids7.png'
 import bids8 from '../../assets/bids8.png'
 import { Link } from 'react-router-dom';
 
-const Bids = ({title}) => {
+const Bids = ({ title }) => {
+  const [isCardShown, setCardShown] = useState(true);
   return (
     <div className='bids section__padding'>
       <div className="bids-container">
@@ -19,30 +20,34 @@ const Bids = ({title}) => {
           <h1>{title}</h1>
         </div>
         <div className="bids-container-card">
-          <div className="card-column" >
-            <div className="bids-card">
-              <div className="bids-card-top">
-                <img src={bids1} alt="" />
-              <Link to={`/item/0`}>
-              <p className="bids-title">Abstact Smoke Red</p>
-              </Link>
-              </div>
-              <div className="bids-card-bottom">
-                <p>1.25 <span>SUI</span></p>
-                <p> <AiFillHeart /> 92</p>
+          {isCardShown &&
+            <div className="card-column" >
+              <div className="bids-card">
+                <div className="bids-card-top">
+                  <img src={bids1} alt="" />
+                  <Link to={`/item/0`}>
+                    <p className="bids-title">Abstact Smoke Red</p>
+                  </Link>
+                </div>
+                <div className="bids-card-bottom">
+                  <p>100 <span>SUI</span></p>
+                  <p> <AiFillHeart /> 92</p>
+                </div>
               </div>
             </div>
-          </div>
+
+          }
+
           <div className="card-column" >
             <div className="bids-card">
               <div className="bids-card-top">
                 <img src={bids2} alt="" />
-              <Link to={`/item/1`}>
-              <p className="bids-title">Mountain Landscape</p>
-              </Link>
+                <Link to={`/item/1`}>
+                  <p className="bids-title">Mountain Landscape</p>
+                </Link>
               </div>
               <div className="bids-card-bottom">
-                <p>0.20 <span>SUI</span></p>
+                <p>100 <span>SUI</span></p>
                 <p> <AiFillHeart /> 25</p>
               </div>
             </div>
@@ -51,12 +56,12 @@ const Bids = ({title}) => {
             <div className="bids-card">
               <div className="bids-card-top">
                 <img src={bids3} alt="" />
-              <Link to={`/item/2`}>
-              <p className="bids-title">Paint Color on Wall</p>
-              </Link>
+                <Link to={`/item/2`}>
+                  <p className="bids-title">Paint Color on Wall</p>
+                </Link>
               </div>
               <div className="bids-card-bottom">
-                <p>0.55 <span>SUI</span></p>
+                <p>100 <span>SUI</span></p>
                 <p> <AiFillHeart /> 55</p>
               </div>
             </div>
@@ -65,12 +70,12 @@ const Bids = ({title}) => {
             <div className="bids-card">
               <div className="bids-card-top">
                 <img src={bids4} alt="" />
-              <Link to={`/item/3`}>
-              <p className="bids-title">Abstract Patern</p>
-              </Link>
+                <Link to={`/item/3`}>
+                  <p className="bids-title">Abstract Patern</p>
+                </Link>
               </div>
               <div className="bids-card-bottom">
-                <p>0.87 <span>SUI</span></p>
+                <p>100 <span>SUI</span></p>
                 <p> <AiFillHeart /> 82</p>
               </div>
             </div>
@@ -79,12 +84,12 @@ const Bids = ({title}) => {
             <div className="bids-card">
               <div className="bids-card-top">
                 <img src={bids5} alt="" />
-              <Link to={`/item/4`}>
-              <p className="bids-title">White Line Grafiti</p>
-              </Link>
+                <Link to={`/item/4`}>
+                  <p className="bids-title">White Line Grafiti</p>
+                </Link>
               </div>
               <div className="bids-card-bottom">
-                <p>0.09 <span>SUI</span></p>
+                <p>100 <span>SUI</span></p>
                 <p> <AiFillHeart /> 22</p>
               </div>
             </div>
@@ -93,12 +98,12 @@ const Bids = ({title}) => {
             <div className="bids-card">
               <div className="bids-card-top">
                 <img src={bids6} alt="" />
-              <Link to={`/item/5`}>
-              <p className="bids-title">Abstract Triangle</p>
-              </Link>
+                <Link to={`/item/5`}>
+                  <p className="bids-title">Abstract Triangle</p>
+                </Link>
               </div>
               <div className="bids-card-bottom">
-                <p>0.90 <span>SUI</span></p>
+                <p>100 <span>SUI</span></p>
                 <p> <AiFillHeart /> 71</p>
               </div>
             </div>
@@ -107,12 +112,12 @@ const Bids = ({title}) => {
             <div className="bids-card">
               <div className="bids-card-top">
                 <img src={bids7} alt="" />
-              <Link to={`/item/6`}>
-              <p className="bids-title">Lake Landscape</p>
-              </Link>
+                <Link to={`/item/6`}>
+                  <p className="bids-title">Lake Landscape</p>
+                </Link>
               </div>
               <div className="bids-card-bottom">
-                <p>0.52 <span>SUI</span></p>
+                <p>100 <span>SUI</span></p>
                 <p> <AiFillHeart /> 63</p>
               </div>
             </div>
@@ -121,12 +126,12 @@ const Bids = ({title}) => {
             <div className="bids-card">
               <div className="bids-card-top">
                 <img src={bids8} alt="" />
-              <Link to={`/item/7`}>
-              <p className="bids-title">Blue Red Art</p>
-              </Link>
+                <Link to={`/item/7`}>
+                  <p className="bids-title">Blue Red Art</p>
+                </Link>
               </div>
               <div className="bids-card-bottom">
-                <p>0.85 <span>SUI</span></p>
+                <p>100 <span>SUI</span></p>
                 <p> <AiFillHeart /> 66</p>
               </div>
             </div>
